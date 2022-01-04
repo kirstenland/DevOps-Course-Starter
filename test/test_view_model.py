ac3397d5aade774302cf3cc278203660e5dcdd53
Kirsty <kirsty.land@softwire.com>
2022-01-04 15:03:38 +0000

    Exercise 3: Add method to separate out to-do-items

--- test/test_view_model.py
+++ test/test_view_model.py
@@ -8,3 +8,15 @@ def test_items_are_stored_in_view_model():
     view_model = ViewModel(items)
     assert view_model.items == items
 
+
+def test_to_do_items_returns_empty_list_if_all_items_are_done():
+    items = [Item(1, 'Test Item', 'Done'), Item(2, 'Another Test Item', 'Done')]
+    view_model = ViewModel(items)
+    assert view_model.to_do_items == []
+
+
+def test_to_do_items_returns_only_items_with_status_to_do_in_order():
+    items = [Item(1, 'Test Item', 'To Do'), Item(2, 'Another Test Item', 'Done'), Item(3, 'A third Test Item', 'To Do')]
+    view_model = ViewModel(items)
+    assert view_model.to_do_items == [items[0], items[2]]
+
--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -5,3 +5,8 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+    
+    @property
+    def to_do_items(self):
+        return [item for item in self._items if item.status == 'To Do']
+
