29b92324b5c5465eff3c7576922adf642c42bebb
Kirsty <kirsty.land@softwire.com>
2022-01-04 15:06:39 +0000

    Exercise 3: Add method to get doing items

--- test/test_view_model.py
+++ test/test_view_model.py
@@ -9,14 +9,31 @@ def test_items_are_stored_in_view_model():
     assert view_model.items == items
 
 
+def test_to_do_items_returns_only_items_with_status_to_do_in_order():
+    items = [Item(1, 'Test Item', 'To Do'), Item(2, 'Another Test Item', 'Done'), Item(3, 'A third Test Item', 'To Do')]
+    view_model = ViewModel(items)
+    assert view_model.to_do_items == [items[0], items[2]]
+
+
 def test_to_do_items_returns_empty_list_if_all_items_are_done():
     items = [Item(1, 'Test Item', 'Done'), Item(2, 'Another Test Item', 'Done')]
     view_model = ViewModel(items)
     assert view_model.to_do_items == []
 
 
-def test_to_do_items_returns_only_items_with_status_to_do_in_order():
-    items = [Item(1, 'Test Item', 'To Do'), Item(2, 'Another Test Item', 'Done'), Item(3, 'A third Test Item', 'To Do')]
+def test_doing_items_returns_only_items_with_status_doing_in_order():
+    items = [
+        Item(1, 'Test Item', 'To Do'),
+        Item(2, 'Another Test Item', 'Done'),
+        Item(3, 'A third Test Item', 'Doing'),
+        Item(3, 'A final Test Item', 'Doing'),
+    ]
     view_model = ViewModel(items)
-    assert view_model.to_do_items == [items[0], items[2]]
+    assert view_model.doing_items == [items[2], items[3]]
+
+
+def test_doing_items_returns_empty_list_if_all_items_are_done_or_to_do():
+    items = [Item(1, 'Test Item', 'To Do'), Item(2, 'Another Test Item', 'Done')]
+    view_model = ViewModel(items)
+    assert view_model.doing_items == []
 
--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -10,3 +10,6 @@ class ViewModel:
     def to_do_items(self):
         return [item for item in self._items if item.status == 'To Do']
 
+    @property
+    def doing_items(self):
+        return [item for item in self._items if item.status == 'Doing']
