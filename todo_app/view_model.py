7427110dccf521264ff80dfaa9f538e31aba8290
Kirsty <kirsty.land@softwire.com>
2022-01-21 15:07:21 +0000

    Exercise 3: Update template to only display recently done items

--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -32,3 +32,7 @@ class ViewModel:
     @property
     def older_done_items(self):
         return [item for item in self.done_items if item.last_modified.date() != date.today()]
+    
+    @property
+    def done_items_always_shown(self):
+        return self.done_items if self.should_show_all_done_items else self.recent_done_items
